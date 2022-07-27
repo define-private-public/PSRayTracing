01a84d813e15967ab62ea6c4f449b005877d179f
Benjamin N. Summerton <def.pri.pub@gmail.com>
2022-07-26 23:39:26 -0400

    Add German Strings

---
+++ qt_ui/i18n/qt_ui_de_DE.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>Messages</name>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="12"/>
+        <source>PSRayTracing (GUI Edition)</source>
+        <translation>PSRayTracing (GUI Ausgabe)</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="13"/>
+        <source>Scene:</source>
+        <translation>Szene:</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="14"/>
+        <source>Stop</source>
+        <translation>Stoppen</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="15"/>
+        <source>Render</source>
+        <translation>Rendern</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="16"/>
+        <source>Total Render Time: %1</source>
+        <translation>Gesamte Renderzeit: %1</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="19"/>
+        <source>Render Settings</source>
+        <translation>Renderneinstellungen</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="20"/>
+        <source>Render Size</source>
+        <translation>Rendergröße</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="21"/>
+        <source>W (min. 1)</source>
+        <translatorcomment>&quot;Breite&quot; is german for &quot;Width&quot;</translatorcomment>
+        <translation>B (min. 1)</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="22"/>
+        <source>H (min. 1)</source>
+        <translatorcomment>&quot;Höhe&quot; is German for &quot;height&quot;</translatorcomment>
+        <translation>H (min. 1)</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="23"/>
+        <source>Use Device Resolution:
+%1</source>
+        <translation>Benutzen
+Bildschirmauflösung:
+%1</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="24"/>
+        <source>Samples Per Pixel</source>
+        <translation>Samples pro Pixel</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="25"/>
+        <source>(minimum value of 1)</source>
+        <translation>(Minimaler Vert 1)</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="26"/>
+        <source>Max Ray Depth</source>
+        <translation>Strahlentiefe</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="27"/>
+        <source>Number of Threads</source>
+        <translation>Anzahl der Threads</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="28"/>
+        <source>Use Recommended Value: %1</source>
+        <translatorcomment>&quot;Empfehlenswert&quot; is too long in German, needs to be on a second line</translatorcomment>
+        <translation>Benutzen
+Empfehlenswert: %1</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="29"/>
+        <source>Random Seed</source>
+        <translation>Zufälliger Samen</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="30"/>
+        <source>Deep Copy per Thread</source>
+        <translation>Tiefen Kopie pro Thread</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="31"/>
+        <source>Reset To Default</source>
+        <translation>Standardeinstellungen
+zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="32"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="33"/>
+        <source>Please give valid input for each field above.</source>
+        <translation>Ober für jede Feld Gültige Eingabe bitte eingeben.</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="34"/>
+        <source>About PSRayTracing</source>
+        <translation>Über PSRayTracing</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="37"/>
+        <source>Press &quot;Render&quot; to start rendering.
+Adjust settings with the button on the lower left.</source>
+        <translation>Drücken Sie &quot;Rendern&quot; zu Rendern starten.
+Passt Einstellungen mit die untere linke Schaltfläche an.</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="38"/>
+        <source>Rendering...
+%1
+%2%</source>
+        <translation>Rendern...
+%1
+%2%</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="39"/>
+        <source>Stopping Active Render...</source>
+        <translation>Anhalten Active Rendern...</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="42"/>
+        <source>Zoom to Fit</source>
+        <translation>Zur einpassen zoomen</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="43"/>
+        <source>Zoom to 100%</source>
+        <translation>Zur 100% zoomen</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="46"/>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="47"/>
+        <source>&lt;p&gt;  PSRayTracing is an implementation of &lt;a href=&quot;https://twitter.com/Peter_shirley&quot;&gt;Peter Shirley&apos;s&lt;/a&gt; &lt;a href=&quot;https://raytracing.github.io/&quot;&gt;Ray Racing in one  Weekend book series.&lt;/a&gt;  While keeping the same architectural structure of the rendering logic, it has alterations and improvements to its project layout,  algorithms, and portability; all while being done in vanilla C++ 17.  On average, it can render in 1/4 of the time as the reference code from the book.  It was  an exercise too in how the ordering of code can affect the compiled output assembly and the performance of a program.  The CMake build system also features the  ability to toggle on (and off) the changes from the books code, as to see the performance impact.&lt;/p&gt;&lt;br&gt;&lt;p&gt;  This app itself is a GUI frontend for the ray tracer.  Written in C++ &amp;amp; Qt 6, it also acts as a way to (easily) test the performance of the ray tracer on  platforms such as Android and iOS.  The scenes from books 1, 2, &amp;amp; 3 are available in this app (plus some bonus ones too).&lt;/p&gt;&lt;br&gt;&lt;p&gt;  The project is open source with it&apos;s code freely available here:&lt;br&gt;  &lt;a href=&quot;https://github.com/define-private-public/PSRayTracing&quot;&gt;https://github.com/define-private-public/PSRayTracing&lt;/a&gt;&lt;/p&gt;&lt;br&gt;&lt;p&gt;  Read More:  &lt;ul&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/psraytracing-a-revisit-of-the-peter-shirley-minibooks-4-years-later/&quot;&gt;PSRayTracing, A revisit of the Peter Shirley Minibooks 4 years later&lt;/a&gt;&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/automated-testing-of-a-ray-tracer/&quot;&gt;Automated Testing of a Ray Tracer&lt;/a&gt;&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/making-a-cross-platform-mobile-desktop-app-with-qt-62/&quot;&gt;Making A Cross Platform Mobile &amp; Desktop App with Qt 6.2&lt;/a&gt;&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/ray-tracing-book-series-review-nim-first-impressions/&quot;&gt;Ray Tracing in Nim&lt;/a&gt;&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;&lt;p&gt;  Thanks:  &lt;ul&gt;    &lt;li&gt;Peter Shirley&lt;/li&gt;    &lt;li&gt;Yining Karl Li&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://github.com/define-private-public/PSRayTracing#thanks&quot;&gt;&amp;amp; more&lt;/a&gt;&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;</source>
+        <translatorcomment>This was translated using DeepL</translatorcomment>
+        <translation>&lt;p&gt;(Dies wurde mit DeepL aus dem Englischen übersetzt)&lt;/p&gt;&lt;p&gt;PSRayTracing ist eine Implementierung von &lt;a href=&quot;https://twitter.com/Peter_shirley&quot;&gt;Peter Shirleys&lt;/a&gt; &lt;a href=&quot;https://raytracing.github.io/&quot;&gt;&amp;quot;Ray Tracing in one Weekend&amp;quot; Buchreihe.&lt;/a&gt; Unter Beibehaltung der gleichen architektonischen Struktur der Rendering-Logik wurden Änderungen und Verbesserungen am Projektlayout, an den Algorithmen und an der Portabilität vorgenommen, und das alles in standardisiert C++ 17. Im Durchschnitt kann es in 1/4 der Zeit gerendert werden als der Referenzcode aus dem Buch. Es war auch eine Übung darin, wie die Reihenfolge des Codes die kompilierte Ausgabe und die Leistung eines Programms beeinflussen kann. Das CMake-Build-System bietet auch die Möglichkeit, die Änderungen aus dem Buchcode zu aktivieren (und zu deaktivieren), um die Auswirkungen auf die Leistung zu sehen.&lt;/p&gt;&lt;br&gt;&lt;p&gt;Diese Anwendung selbst ist ein GUI-Frontend für den Raytracer. Sie wurde in C++ und Qt 6 geschrieben und dient auch dazu, die Leistung des Raytracers auf Plattformen wie Android und iOS (einfach) zu testen. Die Szenen aus den Büchern 1, 2 und 3 sind in dieser App verfügbar (plus einige Bonus-Szenen).&lt;/p&gt;&lt;br&gt;&lt;p&gt;Das Projekt ist quelloffen, der Code ist hier frei verfügbar:&lt;br&gt;&lt;a href=&quot;https://github.com/define-private-public/PSRayTracing&quot;&gt;https://github.com/define-private-public/PSRayTracing&lt;/a&gt;&lt;/p&gt;&lt;br&gt;&lt;p&gt;Mehr lesen (auf Englisch):&lt;ul&gt; &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/psraytracing-a-revisit-of-the-peter-shirley-minibooks-4-years-later/&quot;&gt;PSRayTracing, A revisit of the Peter Shirley Minibooks 4 years later&lt;/a&gt;&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/automated-testing-of-a-ray-tracer/&quot;&gt;Automated Testing of a Ray Tracer&lt;/a&gt;&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/making-a-cross-platform-mobile-desktop-app-with-qt-62/&quot;&gt;Making A Cross Platform Mobile &amp; Desktop App with Qt 6.2&lt;/a&gt;&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/ray-tracing-book-series-review-nim-first-impressions/&quot;&gt;Ray Tracing in Nim&lt;/a&gt;&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;p&gt;Danke:&lt;ul&gt; &lt;li&gt;Peter Shirley&lt;/li&gt;&lt;li&gt;Yining Karl Li&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://github.com/define-private-public/PSRayTracing#thanks&quot;&gt;und andere&lt;/a&gt;&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="85"/>
+        <source>
+&lt;p&gt;  Info:  &lt;ul&gt;    &lt;li&gt;Render Library Version -- %1&lt;/li&gt;    &lt;li&gt;Qt UI (App) Version -- %2&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;</source>
+        <translation>
+&lt;p&gt;  Info:  &lt;ul&gt;    &lt;li&gt;Rendernprogrammbibliothek Version -- %1&lt;/li&gt;    &lt;li&gt;Qt UI (App) Version -- %2&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
