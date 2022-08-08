155b311224f6b2e81a34007cd0b796a1ad4f98e1
Benjamin N. Summerton <def.pri.pub@gmail.com>
2022-08-08 11:56:48 -0400

    Adding a Japanese translation
    Strings & CMake stuff.

---
+++ qt_ui/i18n/qt_ui_ja_JP.ts
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>Messages</name>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="12"/>
+        <source>PSRayTracing (GUI Edition)</source>
+        <translation type="unfinished">PSRayTracing （GUI版）</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="13"/>
+        <source>Scene:</source>
+        <translation type="unfinished">シーン：</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="14"/>
+        <source>Stop</source>
+        <translation type="unfinished">停止する</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="15"/>
+        <source>Render</source>
+        <translation type="unfinished">レンダーをする</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="16"/>
+        <source>Total Render Time: %1</source>
+        <translation type="unfinished">全体のレンダー時間： %1</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="19"/>
+        <source>Render Settings</source>
+        <translation type="unfinished">レンダー設定</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="20"/>
+        <source>Render Size</source>
+        <translation type="unfinished">レンダー解像度</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="21"/>
+        <source>W (min. 1)</source>
+        <translation type="unfinished">幅&#x3000;（最小値＝１）</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="22"/>
+        <source>H (min. 1)</source>
+        <translation type="unfinished">高さ&#x3000;（最小値＝１）</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="23"/>
+        <source>Use Device Resolution:
+%1</source>
+        <translation type="unfinished">ディスプレイ解像度を使用:
+%1</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="24"/>
+        <source>Samples Per Pixel</source>
+        <translation type="unfinished">ピクセル毎のサンプル数</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="25"/>
+        <source>(minimum value of 1)</source>
+        <translation type="unfinished">（最小値は１です）</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="26"/>
+        <source>Max Ray Depth</source>
+        <translation type="unfinished">最大レイの深度</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="27"/>
+        <source>Number of Threads</source>
+        <translation type="unfinished">スレッド数</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="28"/>
+        <source>Use Recommended Value: %1</source>
+        <translation type="unfinished">お勧め値を使用: %1</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="29"/>
+        <source>Random Seed</source>
+        <translation type="unfinished">ランダムシード</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="30"/>
+        <source>Deep Copy per Thread</source>
+        <translation type="unfinished">スレッド毎日ディープコピー</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="31"/>
+        <source>Reset To Default</source>
+        <translation type="unfinished">デフォルト設定をリセット</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="32"/>
+        <source>OK</source>
+        <translation type="unfinished">OK</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="33"/>
+        <source>Please give valid input for each field above.</source>
+        <translation type="unfinished">上に、各エントリーは有効な入力を入れて下さい。</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="34"/>
+        <source>About PSRayTracing</source>
+        <translation type="unfinished">PSRayTracingについて</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="37"/>
+        <source>Press &quot;Render&quot; to start rendering.
+Adjust settings with the button on the lower left.</source>
+        <translation type="unfinished">レンダーを始まってのは、「レンダーをする」を押してます。
+設定を調整のに、 左下のボトンを押してます。</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="38"/>
+        <source>Rendering...
+%1
+%2%</source>
+        <translation type="unfinished">レンダーをしているところです...
+%1
+%2%</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="39"/>
+        <source>Stopping Active Render...</source>
+        <translation type="unfinished">アクティブレンダーを停止しているところです...</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="42"/>
+        <source>Zoom to Fit</source>
+        <translation type="unfinished">全体にズーム</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="43"/>
+        <source>Zoom to 100%</source>
+        <translation type="unfinished">１００％にズーム</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="46"/>
+        <source>Close</source>
+        <translation type="unfinished">閉じる</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="47"/>
+        <source>&lt;p&gt;  PSRayTracing is an implementation of &lt;a href=&quot;https://twitter.com/Peter_shirley&quot;&gt;Peter Shirley&apos;s&lt;/a&gt; &lt;a href=&quot;https://raytracing.github.io/&quot;&gt;Ray Racing in one  Weekend book series.&lt;/a&gt;  While keeping the same architectural structure of the rendering logic, it has alterations and improvements to its project layout,  algorithms, and portability; all while being done in vanilla C++ 17.  On average, it can render in 1/4 of the time as the reference code from the book.  It was  an exercise too in how the ordering of code can affect the compiled output assembly and the performance of a program.  The CMake build system also features the  ability to toggle on (and off) the changes from the books code, as to see the performance impact.&lt;/p&gt;&lt;br&gt;&lt;p&gt;  This app itself is a GUI frontend for the ray tracer.  Written in C++ &amp;amp; Qt 6, it also acts as a way to (easily) test the performance of the ray tracer on  platforms such as Android and iOS.  The scenes from books 1, 2, &amp;amp; 3 are available in this app (plus some bonus ones too).&lt;/p&gt;&lt;br&gt;&lt;p&gt;  The project is open source with it&apos;s code freely available here:&lt;br&gt;  &lt;a href=&quot;https://github.com/define-private-public/PSRayTracing&quot;&gt;https://github.com/define-private-public/PSRayTracing&lt;/a&gt;&lt;/p&gt;&lt;br&gt;&lt;p&gt;  Read More:  &lt;ul&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/psraytracing-a-revisit-of-the-peter-shirley-minibooks-4-years-later/&quot;&gt;PSRayTracing, A revisit of the Peter Shirley Minibooks 4 years later&lt;/a&gt;&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/automated-testing-of-a-ray-tracer/&quot;&gt;Automated Testing of a Ray Tracer&lt;/a&gt;&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/making-a-cross-platform-mobile-desktop-app-with-qt-62/&quot;&gt;Making A Cross Platform Mobile &amp; Desktop App with Qt 6.2&lt;/a&gt;&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/ray-tracing-book-series-review-nim-first-impressions/&quot;&gt;Ray Tracing in Nim&lt;/a&gt;&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;&lt;p&gt;  Thanks:  &lt;ul&gt;    &lt;li&gt;Peter Shirley&lt;/li&gt;    &lt;li&gt;Yining Karl Li&lt;/li&gt;    &lt;li&gt;&lt;a href=&quot;https://github.com/define-private-public/PSRayTracing#thanks&quot;&gt;&amp;amp; more&lt;/a&gt;&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;</source>
+        <translatorcomment>This was translated using DeepL</translatorcomment>
+        <translation type="unfinished">&lt;p&gt;(これはDeepLを使って英語から翻訳したものです。)&lt;/p&gt;&lt;br&gt;&lt;p&gt;「PSRayTracing」は、&lt;a href=&quot;https://twitter.com/Peter_shirley&quot;&gt;Peter Shirley様&lt;/a&gt;の「&lt;a href=&quot;https://raytracing.github.io/&quot;&gt;Ray Racing in one Weekend&lt;/a&gt;」シリーズを実装したものです。レンダリングロジックのアーキテクチャはそのままに、プロジェクトのレイアウト、アルゴリズム、移植性を変更・改善したもので、全て vanilla C++ 17 で作成されています。平均して、この本のリファレンス・コードの1/4の時間でレンダリングすることができます。コードの順序が、コンパイルされた出力アセンブリとプログラムのパフォーマンスにどのような影響を与えるか、これもまた訓練となりました。CMakeのビルドシステムでは、書籍のコードからの変更をオン（オフ）にして、パフォーマンスへの影響を見ることができるのも特徴です。&lt;/p&gt;&lt;br&gt;&lt;p&gt;このアプリ自体は、レイトレーサーのGUIフロントエンドです。C++ と Qt 6 で書かれており、Android や iOS などのプラットフォームでレイトレーサーのパフォーマンスを（簡単に）テストする方法としても機能します。このアプリでは、書籍1、2、3のシーンが利用できます（さらに、いくつかのボーナスもあります）。&lt;/p&gt;&lt;br&gt;&lt;p&gt;このプロジェクトはオープンソースで、コードはここから自由に入手できます：&lt;br&gt;&lt;a href=&quot;https://github.com/define-private-public/PSRayTracing&quot;&gt;https://github.com/define-private-public/PSRayTracing&lt;/a&gt;&lt;/p&gt;&lt;br&gt;&lt;p&gt;もっと読む（英語で）：&lt;ul&gt; &lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/psraytracing-a-revisit-of-the-peter-shirley-minibooks-4-years-later/&quot;&gt;PSRayTracing, A revisit of the Peter Shirley Minibooks 4 years later&lt;/a&gt;&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/automated-testing-of-a-ray-tracer/&quot;&gt;Automated Testing of a Ray Tracer&lt;/a&gt;&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/making-a-cross-platform-mobile-desktop-app-with-qt-62/&quot;&gt;Making A Cross Platform Mobile &amp; Desktop App with Qt 6.2&lt;/a&gt;&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://16bpp.net/blog/post/ray-tracing-book-series-review-nim-first-impressions/&quot;&gt;Ray Tracing in Nim&lt;/a&gt;&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;&lt;p&gt;ありがとうございます：&lt;ul&gt; &lt;li&gt;Peter Shirley&lt;/li&gt;&lt;li&gt;Yining Karl Li&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;https://github.com/define-private-public/PSRayTracing#thanks&quot;&gt;とその他の方々&lt;/a&gt;&lt;/li&gt;&lt;/ul&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../imports/SixteenBPP/Messages.qml" line="85"/>
+        <source>
+&lt;p&gt;  Info:  &lt;ul&gt;    &lt;li&gt;Render Library Version -- %1&lt;/li&gt;    &lt;li&gt;Qt UI (App) Version -- %2&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;</source>
+        <translation type="unfinished">
+&lt;p&gt;  情報:  &lt;ul&gt;    &lt;li&gt;レンダーライブラリのバージョン -- %1&lt;/li&gt;    &lt;li&gt;Qt&#x3000;UI（アプリ）のバージョン -- %2&lt;/li&gt;  &lt;/ul&gt;&lt;/p&gt;</translation>
+    </message>
+</context>
+</TS>
